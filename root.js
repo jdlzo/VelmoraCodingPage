7f9cd5a5d7b31f4d0b71d992421e00b25ae7736e
Jordan Damian Laz Ordoñez <jordan3271@hotmail.com>
2026-09-20 10:10:41 -0500

    fix 404

--- root.js
+++ root.js
@@ -13,7 +13,7 @@ document.addEventListener("click", (e) => {
 // create an object that maps the url to the template, title, and description
 const urlRoutes = {
 	404: {
-		template: "/vis-404.html",
+		template: "/vis-principal.html",
 		title: "404 | " + urlPageTitle,
 		description: "Page not found",
 	},
