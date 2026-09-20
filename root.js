628b5b370e6f842d99456278208a6fdefaa6b6a2
Jordan Damian Laz Ordoñez <jordan3271@hotmail.com>
2026-09-20 09:54:53 -0500

    fix 404

--- root.js
+++ root.js
@@ -13,6 +13,7 @@ document.addEventListener("click", (e) => {
 // create an object that maps the url to the template, title, and description
 const urlRoutes = {
 	404: {
+		template: "/principal.html",
 		title: "404 | " + urlPageTitle,
 		description: "Page not found",
 	},
